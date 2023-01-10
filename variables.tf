415e0d30aa5e89b901ff9c1180406e6b12960f13
Abhishek Ankush Bhilare <69209913+AbhishekBhilare@users.noreply.github.com>
2023-01-10 14:03:18 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,26 +1,51 @@
-variable "region" {
-  default     = "ap-southeast-2"
-  description = "AWS region"
+variable "aws_region" {
+       description = "The AWS region to create things in." 
+       default     = "us-east-1" 
 }
 
-variable "cluster_name" {
-  default = "getting-started-eks"
+variable "key_name" { 
+    description = " SSH keys to connect to ec2 instance" 
+    default     =  "Abhishek-keypair-OpenSSH" 
 }
 
+variable "instance_type" { 
+    description = "instance type for ec2" 
+    default     =  "t2.micro" 
+}
 
-variable "map_roles" {
-  description = "Additional IAM roles to add to the aws-auth configmap."
-  type = list(object({
-    rolearn  = string
-    username = string
-    groups   = list(string)
-  }))
+variable "security_group" { 
+    description = "Name of security group" 
+    default     = "jenkins-sgroup-dec-2021" 
+}
 
-  default = [
-    {
-      rolearn  = "arn:aws:iam::272814864966:role/Terraform-role"
-      username = "Terraform-role"
-      groups   = ["system:masters"]
-    },
-  ]
+variable "tag_name" { 
+    description = "Tag Name of for Ec2 instance" 
+    default     = "my-ec2-instance" 
+} 
+variable "ami_id" { 
+    description = "AMI for Ubuntu Ec2 instance" 
+    default     = "ami-0ee23bfc74a881de5" 
+}
+variable "versioning" {
+    type        = bool
+    description = "(Optional) A state of versioning."
+    default     = true
+}
+variable "acl" {
+    type        = string
+    description = " Defaults to private "
+    default     = "private"
+}
+variable "bucket_prefix" {
+    type        = string
+    description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
+    default     = "my-s3bucket-"
+}
+variable "tags" {
+    type        = map
+    description = "(Optional) A mapping of tags to assign to the bucket."
+    default     = {
+        environment = "DEV"
+        terraform   = "true"
+    }
 }
