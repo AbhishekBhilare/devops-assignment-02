bb31c6c8e4195c59de84badf8038034c7d2367c4
Abhishek Ankush Bhilare <69209913+AbhishekBhilare@users.noreply.github.com>
2023-01-10 21:51:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,3 +16,36 @@ module "vpc" {
     Environment = "dev"
   }
 }
+
+  
+  #Create security group with firewall rules
+resource "aws_security_group" "jenkins-terrafrom-2022" {
+  name        = jenkins-terrafrom-2022
+  description = "security group for Ec2 instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ # outbound from jenkis server
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags= {
+    Name = "jenkins-terrafrom-2022"
+  }
+}
